0d0be46e6bdc705be2b05c35d85af9feb04cbdf0
kakaict <canhcung88@gmail.com>
2018-10-21 19:07:40 +0200

    update

--- src/codingame/FindFileInDirectory.java
+++ src/codingame/FindFileInDirectory.java
@@ -3,15 +3,62 @@ package codingame;
 import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 public class FindFileInDirectory {
 
 	public static void main(String[] args) {
 
-		System.out.println("herre: " +findFile2("iTunes Library.itl", "/Users/khaitq/"));
+//		System.out.println("herre: " +findFile2("iTunes Library.itl", "/Users/khaitq/"));
 		
 //		System.out.println("herre: " +findFile("iTunes Library.itl", "/Users/khaitq/"));
 
+		System.out.println(findFileUseQueue(new File("/Users/khaitq/"), "test.mv.db"));
+
+	//	System.out.println(findFileUseStack(new File("/Users/khaitq/"), "test.mv.db"));
+
+	}
+	
+	//Best choice
+	private static String findFileUseQueue(File dir, String fileName) {
+		
+		Queue<File> queues = new LinkedList<File>();
+		
+		queues.add(dir);
+		
+		while (!queues.isEmpty()) {
+			File f = queues.poll();
+			if (f.isDirectory()) {
+				for (File file : f.listFiles()) {
+					queues.add(file);
+				}
+			}
+			if (f.isFile() && f.getName().equals(fileName)) {
+				return f.getAbsolutePath();
+			}
+		}
+		
+		return null;
+	}
+	
+	private static String findFileUseStack(File directory, String fileName) {
+		
+		Stack<File> stacks = new Stack<File>();
+		stacks.add(directory);
+		while (!stacks.isEmpty()) {
+			
+			File f = stacks.pop();
+			if (f.isDirectory()) {
+				for (File file : f.listFiles()) {
+					stacks.push(file);
+				}
+			}
+			if (f.isFile() && f.getName().equals(fileName)) {
+				return f.getAbsolutePath();
+			}
+		}
+		return null;
 	}
 	
 	private static String findFile(String fileName, String directory){
